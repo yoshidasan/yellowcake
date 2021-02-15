6dbf32c7321fd50ea56210081b294ce33cc9e1d5
yoshidasan <78963214+yoshidasan@users.noreply.github.com>
2021-02-15 15:22:26 +0900

    Create home.js
    2021/02/15

---
+++ src/pages/home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import _get from 'lodash/get'
+import AlertTriangle from 'react-feather/dist/icons/alert-triangle'
+
+import Layout from '../components/Layout'
+data => (
+      <Layout>
+        <Helmet>
+          <title>home</title>
+        </Helmet>
+        <section className="section thick">
+          <div className="container skinny taCenter">
+            <p>
+              <AlertTriangle size="5rem" />
+            </p>
+            <h1>test</h1>
+            <p>
+              We can't find the page you are looking for!
+              <br />
+              Head back to{' '}
+              <Link to="/">{_get(data, 'globalSettings.siteTitle')}</Link>
+            </p>
+          </div>
+        </section>
+      </Layout>
+    )}
+  />
+)
